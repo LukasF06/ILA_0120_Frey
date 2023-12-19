169b4d2c806ce2a7c13753b37657b1a2f3a5ba99
Lukas Frey <l.frey.inf21@stud.bbbaden.ch>
2023-12-19 15:44:12 +0100

    Update

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AddNote from './components/AddNote.js';
 import HomeScreen from './components/HomeScreen.js';
-import EditNote from './components/EditNote.js'
 import SearchNote from './components/SearchNote.js'
 
 const Stack = createNativeStackNavigator();
@@ -16,7 +15,6 @@ function App() {
         <Stack.Screen name="Deine Notizen" component={HomeScreen} options={{ title: 'Deine Notizen', headerShown: false, gestureEnabled: false }}/>
         <Stack.Screen name="Notiz erstellen" component={AddNote} options={{ title: 'Notiz erstellen', headerShown: false, gestureEnabled: false, animation:'none' }}/>
         <Stack.Screen name="Suche" component={SearchNote} options={{ title: 'Suche', headerShown: false }}/>
-        <Stack.Screen name="Notiz editieren" component={EditNote} options={{ title: 'Notiz editieren', headerShown: false, gestureEnabled: false, animation: 'none' }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
